ce68b373bb82acb25d5f79edc9f1c5150bc4a48d
nikotastic <velasqueznikol10@gmail.com>
2025-10-14 16:16:59 -0500

    refactor: Restructure Doctor data persistence

--- HospitalCompleteSystem/Data/Database.cs
+++ HospitalCompleteSystem/Data/Database.cs
@@ -5,7 +5,7 @@ namespace HospitalCompleteSystem.Data;
 public static class Database
 {
         public static List<Patient> Patients = new List<Patient>();
-        //public static List<Models.Doctor> Doctors { get; } = new List<Models.Doctor>();
+        public static List<Doctor> Doctors = new List<Doctor>();
         //public static List<Models.Appointment> Appointments { get; } = new List<Appointment>();
         
         // Simulated auto-incrementing IDs
